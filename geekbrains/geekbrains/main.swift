0e600a8ee63a04fb2f1c489310ee66d8cbf0872c
elkhr <vitaminhero.mail@gmail.com>
2019-03-03 08:39:01 +0300

    l

---
+++ geekbrains/geekbrains/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  geekbrains
+//
+//  Created by light on 03/03/2019.
+//  Copyright © 2019 light. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+l3
