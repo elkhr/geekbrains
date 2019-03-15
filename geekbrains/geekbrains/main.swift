6ba34d3d69dec447962f0136aaf5f31bd4514f1e
elkhr <vitaminhero.mail@gmail.com>
2019-03-15 12:07:56 +0300

    sportCar + trunkCar

--- geekbrains/geekbrains/main.swift
+++ geekbrains/geekbrains/main.swift
@@ -79,9 +79,42 @@ class SportCar : Car {
     }
 }
 
+class TrunkCar : Car {
+    var color: String
+    var enginePower: Int
+    var windowState: Window
+    var engineState: Engine
+    var trailer: Bool
+    var weelCondition: Weel
+    func engineOn() {
+        if (engineState == .off) {
+            engineState = .on
+        }
+    }
+    func windowClose() {
+        if (windowState == .open) {
+            windowState = .close
+        }
+    }
+    init(color: String, enginePower: Int, windowState: Window, engineState: Engine, trailer: Bool, weelCondition: Weel){
+        self.color = color
+        self.enginePower = enginePower
+        self.windowState = windowState
+        self.engineState = engineState
+        self.trailer = trailer
+        self.weelCondition = weelCondition
+    }
+}
+
+extension TrunkCar: CustomStringConvertible {
+    var description: String {
+        return "состояние окон грузовика: \(windowState)"
+    }
+}
+
 extension SportCar: CustomStringConvertible {
     var description: String {
-        return "состояние двигателя: \(engineState)"
+        return "состояние двигателя спорткара: \(engineState)"
     }
 }
 
@@ -90,97 +123,8 @@ print(sport)
 sport.engineOn()
 print(sport)
 
-
-/*class trunkCar : Car {
- var color: String = "red"
- var enginePower: Int = 100
- var windowState: Window = .open
- var engineState: Engine = .on
- var trailer: Bool = true
- var weelCondition: Weel = .simpleWeel
- }*/
-
-
-
-
-/*
- enum Transmission {
- case manual, auto
- }
- 
- enum Engine {
- case rotor, V
- }
- 
- enum Turbo {
- case on, off
- }
- 
- enum HookTrailer {
- case hook, unhooked
- }
- 
- class Car {
- let color: String
- let enginePower: Int
- var transmission: Transmission
- 
- init(color: String, enginePower: Int, transmission: Transmission) {
- self.color = color
- self.enginePower = enginePower
- self.transmission = transmission
- }
- func action (){
- if transmission != .manual {
- transmission = .manual
- } else {transmission = .auto}
- }
- }
- 
- class SportCar : Car {
- var engine: Engine = .rotor
- var turbo: Turbo = .off
- init(color: String, enginePower: Int, transmission: Transmission, engine: Engine, turbo: Turbo){
- super.init (color: color, enginePower: enginePower, transmission: transmission)
- self.engine = engine
- self.turbo = turbo
- }
- override func action (){
- if turbo != .on {
- turbo = .on
- } else {turbo = .off}
- }
- }
- 
- 
- class TruckCar : Car {
- var trailer: Bool = true
- var hooktrailer: HookTrailer = .unhooked
- init(color: String, enginePower: Int, transmission: Transmission, trailer : Bool, hooktrailer: HookTrailer){
- super.init (color: color, enginePower: enginePower, transmission: transmission)
- self.trailer = trailer
- self.hooktrailer = hooktrailer
- }
- override func action (){
- if hooktrailer != .hook {
- hooktrailer = .hook
- } else {hooktrailer = .unhooked}
- }
- }
- 
- var sport = SportCar (color: "red", enginePower: 900, transmission: .manual, engine: .rotor, turbo: .off)
- print ("Свойства спорткара: цвет:", sport.color, ", мощность (л/с):", sport.enginePower, ", тип трансмиссии:", sport.transmission, ", включение турбо-режима:", sport.turbo)
- 
- sport.action()
- print ("Свойства спорткара после действия: цвет:", sport.color, ", мощность (л/с):", sport.enginePower, ", тип трансмиссии:", sport.transmission, ", включение турбо-режима:", sport.turbo)
- 
- 
- var truck = TruckCar (color: "blue", enginePower: 250, transmission: .auto, trailer: true, hooktrailer: .unhooked)
- print ("Свойства грузовика: цвет:", truck.color, ", мощность (л/с):", truck.enginePower, ", тип трансмиссии:", truck.transmission, ", наличие прицепа:", truck.trailer, ", сцепка прицепа:", truck.hooktrailer)
- 
- truck.action()
- print ("Свойства грузовика после действия: цвет:", truck.color, ", мощность (л/с):", truck.enginePower, ", тип трансмиссии:", truck.transmission, ", наличие прицепа:", truck.trailer, ", сцепка прицепа:", truck.hooktrailer)
-
- */
-
+var trunk = TrunkCar (color: "blue", enginePower: 300, windowState: .open, engineState: .off, trailer: true, weelCondition: .simpleWeel)
+print(trunk)
+trunk.windowClose()
+print(trunk)
 
