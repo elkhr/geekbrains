74f9e43fed5658ba1fc5061c93ca075971951f35
elkhr <vitaminhero.mail@gmail.com>
2019-03-21 15:45:23 +0300

    домашнее задание

--- geekbrains/geekbrains/main.swift
+++ geekbrains/geekbrains/main.swift
@@ -8,6 +8,54 @@
 
 import Foundation
 
+/*1. Придумать класс, методы которого могут создавать непоправимые ошибки. Реализовать их с помощью try/catch.
+ 2. Придумать класс, методы которого могут завершаться неудачей. Реализовать их с использованием Error.*/
+
+enum CarStackError: Error {
+    case invalidSelection
+    case outOfStock
+    }
+
+struct Item {
+    var count: Int
+    let mark: Mark
+}
+
+struct Mark{
+    let name: String
+}
+// Вендинговая машина
+class CarStack {
+    var inventory = [
+        "Ferrary": Item(count: 25, mark: Mark(name: "Ferrary")),
+        "Bently": Item(count: 5, mark: Mark(name: "Bently"))
+    ]
+    func carCheck(itemNamed name: String) throws -> Mark {
+        guard let item = inventory[name] else {
+            throw CarStackError.invalidSelection
+        }
+        guard item.count > 0 else {
+            throw CarStackError.outOfStock
+        }
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+        return newItem.mark
+    }
+}
+
+let carStack = CarStack()
 
 
+do {
+    _ = try carStack.carCheck(itemNamed: "ОКА")
+} catch CarStackError.invalidSelection {
+    print("У нас на складе только Ferrary и Bently")
+}
 
+
+do {
+    _ = try carStack.carCheck(itemNamed: "ОКА")
+} catch let error {
+    print(error.localizedDescription)
+}
